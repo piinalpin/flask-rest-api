4c76555ee55d8aab187e9fb92f7f098a4f31fb85
Alvinditya Saputra <alvindityas@gmail.com>
2019-01-20 05:25:05 +0700

    Add CORS library

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
+from flask_cors import CORS
+
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "db/flask-api.db"))
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
