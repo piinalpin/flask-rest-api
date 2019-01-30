3228df850cd2ca4f29b81659b4b70f371a31b901
Alvinditya Saputra <alvindityas@gmail.com>
2019-01-30 17:40:17 +0700

    change host

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 from app import app
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='flask-rest-api-maverick.herokuapp.com', port=8000, debug=True)
